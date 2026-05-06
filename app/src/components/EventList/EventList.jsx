ebd0652f7102330647757ad12516b833fccf304d
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2026-05-07 00:28:17 +0200

    Fetch API implementation done replacing mock data

--- app/src/components/EventList/EventList.jsx
+++ app/src/components/EventList/EventList.jsx
@@ -1,8 +1,11 @@
-import events from "../../data/events.js";
+// import events from "../../data/events.js";
+import { useEvents } from "../../hooks/useEvents.js";
 import EventCard from "../EventCard/EventCard.jsx";
 import SearchBar from "../SearchBar/SearchBar.jsx";
+import Pagination from "../Pagination/Pagination.jsx"
 import styles from "./EventList.module.css";
 import { useState } from "react";
+import api from "../../api.js";
 
 // TODO: split each event below into its own EventCard component
 // TODO: add a "Buy ticket" button to each event card
@@ -11,23 +14,37 @@ import { useState } from "react";
 export default function EventList() {
 
 const [searchInput, setSearchInput] = useState("");
+const [page, setPage] = useState(1);
+const limit = 6;
+
+const apiUrl = searchInput ? api(`/events?q=${searchInput}&_page=${page}&_limit=${limit}`) : api(`/events?_page=${page}&_limit=${limit}`);
+
+const { data : events, loading, error, totalCount} = useEvents(apiUrl);
+
+const totalPages = Math.ceil(totalCount / limit);
+
+
+if(loading){
+  return <p>Loading events...</p>;
+}
+  
+if(error){
+  return <p>Something went wrong: {error}</p>
+}
+
 
   return (
-
     <>
-    
     <SearchBar searchInput={searchInput} setSearchInput={setSearchInput}/>
     
     <ul className={styles.list}>
-      {events
-        .filter((event) =>
-          event.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-          event.city.toLowerCase().includes(searchInput.toLowerCase())
-        )
-        .map((event) => (
+      {events.map((event) => (
           <EventCard key={event.id} event={event}/>
         ))}
     </ul>
+
+    <Pagination page={page} setPage={setPage} totalPages={totalPages}/>
+
     </>
 
   );
